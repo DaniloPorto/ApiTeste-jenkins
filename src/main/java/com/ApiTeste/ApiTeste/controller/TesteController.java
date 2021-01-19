add3a6bc3a68d73ad6c1784b0787511f3dcb919f
danilo.aparecido <danilo.aparecido@totvs.com.br>
2021-01-18 22:27:58 -0300

    Configuração do projeto

---
+++ src/main/java/com/ApiTeste/ApiTeste/controller/TesteController.java
@@ -0,0 +1,17 @@
+package com.ApiTeste.ApiTeste.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/")
+public class TesteController {
+	
+	@GetMapping
+	public ResponseEntity<String> find() {
+		return ResponseEntity.ok().body("Teste concluído com sucesso!");
+	}
+
+}
